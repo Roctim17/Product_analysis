031177420de1507f3e6a68546f2f1e30e6d41d63
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-04-04 14:27:07 +0600

    404 page setup

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h1>WelCome to my Home Page</h1>
+            <div className="home-container">
+                <div className="image-area">
+                    <img src="image/nike-bat.jpg" alt="" />
+                </div>
+                <div className="text-area">
+                    <h1>Nike Cricket Bat</h1>
+                    <h1>English Willow Master Edition</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In commodi autem quisquam at magnam, illum reiciendis placeat maiores ea hic? Quod delectus incidunt velit nihil.</p>
+                    <button className='btn-live'>Live demo</button>
+                </div>
+            </div>
+            <div className="top-review-container">
+                <h1>Customer Reviews</h1>
+                <button>See All Reviews</button>
+            </div>
         </div>
     );
 };
