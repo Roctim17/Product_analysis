b6792856980867bdebd3229d76bbb677a6d0f811
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-04-05 11:19:45 +0600

    make reaponsive

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomLink from '../CustomLink/CustomLink';
 import "./Header.css"
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div>
-            <nav className='nav'>
+            <div onClick={() => setOpen(!open)} className="w-6 h-6 ">
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <nav className='nav-header'>
+
                 <h1><CustomLink to="/" >Home</CustomLink></h1>
                 <h1><CustomLink to="/review" >Review</CustomLink></h1>
                 <h1><CustomLink to="/dashboard" >DashBoard</CustomLink></h1>
                 <h1><CustomLink to="/blog" >Blogs</CustomLink></h1>
                 <h1><CustomLink to="/about" >About</CustomLink></h1>
             </nav>
+
+
         </div>
     );
 };
